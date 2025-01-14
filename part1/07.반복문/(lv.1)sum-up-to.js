0763ad02d8d4c7d1a2931a56c63e1d7de739ac57
llddang <bodyness@naver.com>
2025-01-14 09:47:41 +0900

    feat: 07.반복문 관련 문제 해결

--- part1/07.반복문/(lv.1)sum-up-to.js
+++ part1/07.반복문/(lv.1)sum-up-to.js
@@ -11,6 +11,10 @@
  */
 function sumUpTo(n) {
   // TODO: n이 1 이상이면 1부터 n까지의 합을 반환, 0 이하이면 0을 반환
+  if (n <= 0) return 0;
+  return Array(n)
+    .fill(1)
+    .reduce((acc, _, i) => acc + i + 1, 0);
 }
 
 // export를 수정하지 마세요.
--- part1/07.반복문/(lv.1)팩토리얼.js
+++ part1/07.반복문/(lv.1)팩토리얼.js
@@ -10,6 +10,10 @@
  */
 function factorial(n) {
   // TODO: n이 1 이상이면 1부터 n까지의 합을 반환, 0 이하이면 0을 반환
+  if (n <= 0) return 0;
+  let f = 1;
+  for (let i = 2; i <= n; i++) f += i;
+  return f;
 }
 
 // export를 수정하지 마세요.
